c7dc0a8e43fa040630b84520ede97da9d6088b68
livigwinnett <124712098+livigwinnett@users.noreply.github.com>
2025-03-07 15:42:04 -0500

    Update driveTo.py

--- driveTo.py
+++ driveTo.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
-#how to communicate with other file
-#import?
-import rospy, math
+import rospy
+import math
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String  # Import String message type
 
 class Robot:
     def __init__(self):
         rospy.init_node('robot_controller', anonymous=True)
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.rate = rospy.Rate(10)
+        self.target_data = None  # Store received data
+
+        # Subscriber for /processed_aoa_data
+        rospy.Subscriber("/processed_aoa_data", String, self.callback)
 
     def move_to_distance(self, distance):
         outData = Twist()
@@ -44,24 +48,29 @@ class Robot:
         self.rotate_to_angle(target_angle)
         self.move_to_distance(target_distance)
 
-    def callback(data):
-        try:
-            data = msg.data
+    def callback(self, msg):
+        """ Callback function to process incoming data """
+        self.target_data = msg.data
 
 if __name__ == '__main__':
-    rospy.init_node("robot_drive")
-    sub = rospy.Subscriber("/processed_aoa_data", String, callback)
-
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        rate.sleep()
-    
     try:
         myRobot = Robot()
-        data_list = data.split(",")
-        target_angle = -data_list[3]  # Set desired angle in degrees
-        target_distance = data_list[1] # Set desired distance in meters
-        myRobot.go_to_target(target_angle, target_distance)
-        
+        rospy.loginfo("Robot is ready and waiting for target data.")
+
+        while not rospy.is_shutdown():
+            if myRobot.target_data:
+                try:
+                    data_list = myRobot.target_data.split(",")
+                    target_angle = -float(data_list[3])  # Convert to float
+                    target_distance = float(data_list[1])  # Convert to float
+                    rospy.loginfo(f"Moving to angle: {target_angle}, distance: {target_distance}")
+                    myRobot.go_to_target(target_angle, target_distance)
+                    
+                    myRobot.target_data = None  # Reset data after processing
+                except (IndexError, ValueError) as e:
+                    rospy.logerr(f"Error processing data: {e}")
+
+            myRobot.rate.sleep()
+
     except rospy.ROSInterruptException:
         pass
