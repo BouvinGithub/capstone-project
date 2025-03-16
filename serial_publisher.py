e54ad441816544deed0d847322647453e0ec62c4
BouvinGithub <155662148+BouvinGithub@users.noreply.github.com>
2025-03-16 12:56:31 -0400

    Update serial_publisher.py

--- serial_publisher.py
+++ serial_publisher.py
@@ -9,12 +9,22 @@ def serial_publisher():
     ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
     rate = rospy.Rate(10)
 
-    while not rospy.is_shutdown():
-        if ser.in_waiting > 0:
-            line = ser.readline().decode("utf-8").strip()
-            print(line)
-            pub.publish(line)
-        rate.sleep()
+    skip = 6
+    count = 0
+    try:
+        while not rospy.is_shutdown():
+            if ser.in_waiting > 0:
+                line = ser.readline().decode("utf-8").strip()
+                if count <= skip:
+                    pub.publish(line)
+                else:
+                    count = 0
+                count += 1
+            rate.sleep()
+    finally:
+        print("shutting down serial publisher")
+        ser.close()
+        
 if __name__ == '__main__':
     try:
         serial_publisher()
